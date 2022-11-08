a658266d482a0698b90bcff2c64499d133eb7424
Fahim-05 <fahim45147@gmail.com>
2022-11-08 11:34:29 +0600

    footer added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+          <h1>this is home</h1>
+
+
+          
+          <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo/logo.png';
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-200">
                 <div className="navbar-start">
-                    
+                    <img className='w-12' src={logo} alt=''/>
                     <a className="btn btn-ghost normal-case text-2xl text-violet-600 font-bold">Wild <span className='mx-2 text-violet-400'>vs.</span> Camera</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0">
+                    <ul className="menu menu-horizontal p-0 text-green-700">
                         <li><Link>Home</Link></li>
                         <li><Link>Services</Link></li>
                         <li><Link>My Reviews</Link></li>
@@ -19,7 +20,7 @@ const Header = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Get started</a>
+                    <button className='btn'></button>
                 </div>
             </div>
         </div>
